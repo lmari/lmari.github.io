40ea39fb547364e0fd4e66fe0f2019334009c39a
Luca Mari <lmari@liuc.it>
2024-03-07 12:11:16 +0100

    attivita updates

--- chatting/attivita/linearutils.py
+++ chatting/attivita/linearutils.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
+import torch
 
 def get_params(model, rounding=None):
     return [round(p.item(), rounding) if rounding else p.item() for p in reversed(list(model.parameters()))]
+
 
 def plot(x, y, title=None, model=None):
     plt.close('all')
@@ -13,3 +15,15 @@ def plot(x, y, title=None, model=None):
         [k0, k1] = get_params(model)
         plt.plot(xx, k0 + k1 * xx)
     plt.show()
+
+
+def train(model, criterion, optimizer, x, y, num_epochs=100):
+    print("epoca\tparametri\tMSE")
+    for epoch in range(num_epochs):
+        y1 = model(x)
+        loss = criterion(y1, y)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        mse = torch.mean((y1 - y)**2)
+        print(f"{epoch+1}\t{get_params(model, 2)}\t{round(mse.item(), 2)}")
