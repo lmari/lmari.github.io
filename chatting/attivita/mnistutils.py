d7aab58fa982c21d865bac127bb9cb8c50741508
Luca Mari <lmari@liuc.it>
2024-04-13 18:34:48 +0200

    attivita updates

--- chatting/attivita/mnistutils.py
+++ chatting/attivita/mnistutils.py
@@ -7,16 +7,17 @@ from torch.utils.data import DataLoader
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 data_folder = "data"                    # la cartella in cui sono mantenute le immagini
 
-def load_data():
+def load_data() -> tuple:
+    ''' Carica i dati di addestramento e di test. '''
     train_data = datasets.MNIST(root=data_folder, train=True, download=True, transform=ToTensor())
     test_data = datasets.MNIST(root=data_folder, train=False, download=True, transform=ToTensor())
     return train_data, test_data
 
 
-def show_images(test_data):
+def show_images(test_data) -> None:
+    ''' Visualizza 10 immagini scelte a caso. '''
     plt.figure(figsize=(10, 4))
     for i in range(1, 11):
         X, y = test_data[random.randint(0, len(test_data))]
@@ -27,7 +28,8 @@ def show_images(test_data):
     plt.show()
 
 
-def train_loop(dataset, batch_size, num_batches, network, loss_fn, optimizer):
+def train_loop(dataset, batch_size, num_batches, network, loss_fn, optimizer) -> float:
+    ''' Addestramento di base. '''
     network.train()                     # metti in modalità di addestramento
     dataloader = iter(DataLoader(dataset, batch_size=batch_size, shuffle=True))
     for i in range(num_batches):        # per ogni batch:
@@ -40,7 +42,8 @@ def train_loop(dataset, batch_size, num_batches, network, loss_fn, optimizer):
     return loss.item()                  # restituisci l'errore
 
 
-def test_loop(dataset, batch_size, network):    # inferenza di base 
+def test_loop(dataset, batch_size, network) -> float:
+    ''' Inferenza di base. '''
     network.eval()                              # metti in modalità di test
     dataloader = iter(DataLoader(dataset, batch_size=batch_size, shuffle=True))
     with torch.no_grad():                       # non calcolare i gradienti
@@ -50,14 +53,16 @@ def test_loop(dataset, batch_size, network):    # inferenza di base
     return correct
 
 
-def test_with_prob(test_data, network):         # inferenza su 1 immagine scelta a caso e visualizzazione del risultato
+def test_with_prob(test_data, network) -> None:
+    ''' Inferenza su 1 immagine scelta a caso e visualizzazione del risultato. '''
     network.eval()                              # metti in modalità di test
     X, y = test_data[random.randint(0, len(test_data))]
+    X = X.unsqueeze(0)                          # aggiunge la dimensione del batch (necessario per la parte convoluzionale, prima veniva fatto automaticamente da dataloader)
     with torch.no_grad():                       # non calcolare i gradienti
         logits = network(X)                     # calcola la previsione
-    pred_probab = nn.Softmax(dim=1)(logits)     # converti la previsione in una distribuzione di probabilità
+    pred_probab = nn.Softmax(dim=1)(logits)     # converti la previsione in una distribuzione di probabilità (con la SoftMax)
     pred_probab_np = pred_probab.cpu().detach().numpy()[0]
-    y_pred = pred_probab.argmax(1)              # trova la classe più probabile      
+    y_pred = pred_probab.argmax(1)              # trova la classe più probabile         
     classes = np.arange(10)
     fig, axs = plt.subplots(1, 2, figsize=(8,3), gridspec_kw={'width_ratios':[1,4]})
     fig.tight_layout(pad=5.0)
@@ -72,12 +77,14 @@ def test_with_prob(test_data, network):         # inferenza su 1 immagine scelta
     plt.show()
 
 
-def test_loop_with_trace(test_data, network):   # inferenza su 10 immagini scelte a caso e visualizzazione del risultato
+def test_loop_with_trace(test_data, network) -> None:
+    ''' Inferenza su 10 immagini scelte a caso e visualizzazione del risultato. '''
     network.eval()
     plt.figure(figsize=(10, 4))
     with torch.no_grad():
         for i in range(1, 11):
             X, y = test_data[random.randint(0, len(test_data))]
+            X = X.unsqueeze(0)                  
             pred = network(X)
             plt.subplot(1, 10, i)
             plt.title(f"prev: {pred.argmax(1).item()}\ncorr: {y}")
